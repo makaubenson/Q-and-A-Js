7b98f22104967c9c89269b4aa5b8a455937ab2b2
Benson <bensonmakau2000@gmail.com>
2021-12-22 20:56:49 +0300

    Created a Q and A js project where the user can expand and close the question after reading. The questions were dynamically programmed to close when one of them is open.

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-//using selectors inside the element
-// traversing the dom
+//Method 1 - using selectors inside the element
+const questions = document.querySelectorAll(".question");
+questions.forEach(function (question) {
+  //   console.log(question);
+  const btn = question.querySelector(".question-btn");
+  //   console.log(btn);
+  btn.addEventListener("click", function () {
+    //close all questions opened after opening a new one
+    questions.forEach(function (item) {
+      //   console.log(item);
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+    question.classList.toggle("show-text");
+  });
+});
+// METHOD 2 -Traversing the DOM
+// const btns = document.querySelectorAll(".question-btn");
+// btns.forEach(function (button) {
+//   button.addEventListener("click", function (e) {
+//     // console.log(e.currentTarget.parentElement.parentElement);
+//     const quiz = e.currentTarget.parentElement.parentElement; //traversing through the DOM
+//     quiz.classList.toggle("show-text");
+//   });
+// });
